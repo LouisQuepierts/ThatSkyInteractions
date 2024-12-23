c6b7d0fafc02f57e26a6cc484ebc0ec1fbbd4851
Louis_Quepierts <LouisQuepierts@outlook.com>
2024-12-24 00:17:48 +0800

    21.3.6-2024.12.24-00h: Remove redundant render targets in cloud.json Change mixin position for UiCombiner.TOP

--- src/main/java/net/quepierts/thatskyinteractions/mixin/GameRendererMixin.java
+++ src/main/java/net/quepierts/thatskyinteractions/mixin/GameRendererMixin.java
@@ -3,25 +3,21 @@ package net.quepierts.thatskyinteractions.mixin;
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.gui.GuiGraphics;
-import net.minecraft.client.gui.MapRenderer;
 import net.minecraft.client.renderer.GameRenderer;
 import net.quepierts.thatskyinteractions.client.gui.layer.UiCombiner;
 import net.quepierts.thatskyinteractions.client.reference.Shaders;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(GameRenderer.class)
-public abstract class GameRendererMixin {
-    @Shadow public abstract MapRenderer getMapRenderer();
-
+public class GameRendererMixin {
     @Inject(
             method = "resize",
             at = @At("RETURN")
     )
-    public void tsi$resizeGameRenderer(int width, int height, CallbackInfo ci) {
+    private void tsi$resizeGameRenderer(int width, int height, CallbackInfo ci) {
         Shaders.resize(width, height);
     }
 
@@ -29,11 +25,10 @@ public abstract class GameRendererMixin {
             method = "render",
             at = @At(
                     value = "INVOKE",
-                    target = "Lnet/minecraft/client/gui/Gui;render(Lnet/minecraft/client/gui/GuiGraphics;Lnet/minecraft/client/DeltaTracker;)V",
-                    shift = At.Shift.AFTER
+                    target = "Lnet/minecraft/client/gui/Gui;renderSavingIndicator(Lnet/minecraft/client/gui/GuiGraphics;Lnet/minecraft/client/DeltaTracker;)V"
             )
     )
-    public void tsi$drawLayers(DeltaTracker deltaTracker, boolean renderLevel, CallbackInfo ci, @Local GuiGraphics guiGraphics) {
+    private void tsi$drawLayers(DeltaTracker deltaTracker, boolean renderLevel, CallbackInfo ci, @Local GuiGraphics guiGraphics) {
         UiCombiner.TOP.render(guiGraphics, deltaTracker);
     }
 }
