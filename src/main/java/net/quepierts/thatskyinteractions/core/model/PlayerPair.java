1da28a9f0df9d0e0194833a53b4c51f7ef23c690
LouisQuepierts <97670116+LouisQuepierts@users.noreply.github.com>
2026-05-05 19:09:11 +1000

    refactor(model): simplify PlayerPair class using Lombok

--- src/main/java/net/quepierts/thatskyinteractions/core/model/PlayerPair.java
+++ src/main/java/net/quepierts/thatskyinteractions/core/model/PlayerPair.java
@@ -1,29 +1,12 @@
 package net.quepierts.thatskyinteractions.core.model;
 
-import com.mojang.serialization.Codec;
-import com.mojang.serialization.codecs.RecordCodecBuilder;
-import io.netty.buffer.ByteBuf;
-import net.minecraft.core.UUIDUtil;
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.network.FriendlyByteBuf;
-import net.minecraft.network.codec.StreamCodec;
+import lombok.Getter;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.UUID;
 
+@Getter
 public class PlayerPair {
-    public static final Codec<PlayerPair> CODEC = RecordCodecBuilder.create(instance -> instance.group(
-            UUIDUtil.CODEC.fieldOf("left").forGetter(PlayerPair::getLeft),
-            UUIDUtil.CODEC.fieldOf("right").forGetter(PlayerPair::getRight)
-    ).apply(instance, PlayerPair::new));
-
-    public static final StreamCodec<ByteBuf, PlayerPair> STREAM_CODEC = StreamCodec.composite(
-            UUIDUtil.STREAM_CODEC,
-            PlayerPair::getLeft,
-            UUIDUtil.STREAM_CODEC,
-            PlayerPair::getRight,
-            PlayerPair::new
-    );
 
     @NotNull private final UUID left;
     @NotNull private final UUID right;
@@ -49,37 +32,5 @@ public class PlayerPair {
     @Override
     public int hashCode() {
         return left.hashCode() ^ right.hashCode();
-    }
-
-    public void serializeNBT(CompoundTag tag) {
-        tag.putUUID("a", this.left);
-        tag.putUUID("b", this.right);
-    }
-
-    public static PlayerPair deserializeNBT(CompoundTag tag) {
-        UUID a1 = tag.getUUID("a");
-        UUID b1 = tag.getUUID("b");
-        return new PlayerPair(a1, b1);
-    }
-
-    public static PlayerPair fromNetwork(FriendlyByteBuf friendlyByteBuf) {
-        return new PlayerPair(friendlyByteBuf.readUUID(), friendlyByteBuf.readUUID());
-    }
-
-    public @NotNull UUID getLeft() {
-        return left;
-    }
-
-    public @NotNull UUID getRight() {
-        return right;
-    }
-
-    public UUID getOther(UUID uuid) {
-        return left.equals(uuid) ? right : left;
-    }
-
-    public void toNetwork(FriendlyByteBuf friendlyByteBuf) {
-        friendlyByteBuf.writeUUID(this.left);
-        friendlyByteBuf.writeUUID(this.right);
     }
 }
