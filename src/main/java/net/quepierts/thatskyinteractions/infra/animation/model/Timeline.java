ee12a8cd15d12a76d95cdebbc8eaf525505a488a
LouisQuepierts <97670116+LouisQuepierts@users.noreply.github.com>
2026-05-10 20:47:30 +1000

    refactor(animation): replace Segment-based Timeline with optimized record structure

--- src/main/java/net/quepierts/thatskyinteractions/infra/animation/model/Timeline.java
+++ src/main/java/net/quepierts/thatskyinteractions/infra/animation/model/Timeline.java
@@ -1,57 +1,28 @@
 package net.quepierts.thatskyinteractions.infra.animation.model;
 
-import lombok.Getter;
-import net.quepierts.thatskyinteractions.infra.util.ArrayIterator;
-import org.jspecify.annotations.NonNull;
+import lombok.experimental.UtilityClass;
+import net.quepierts.thatskyinteractions.infra.animation.interpolator.Interpolator4f;
 
-import java.util.Iterator;
+public record Timeline(
+        float[]             starts,
+        float[]             ends,
+        int[]               addr0,
+        int[]               addr1,
+        Interpolator4f[]    interpolation,
+        int                 size
+) {
 
-public final class Timeline implements Iterable<Segment> {
-    private final Segment[] segments;
+    @UtilityClass
+    public static class Address {
+        static final int PARAMETER_BIT = 0x80000000;
 
-    @Getter
-    private final int max;
-
-    public Timeline(
-            Segment[] segments,
-            int max
-    ) {
-        this.segments = segments;
-        this.max = max;
-    }
-
-    public Segment get(int index) {
-        return this.segments[index];
-    }
-
-    public Segment next(int index) {
-        return this.segments[Math.min(index + 1, this.max)];
-    }
-
-    public int find(float time) {
-        int left = 0;
-        int right = this.max - 1;
-
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if (time < this.segments[mid].start()) {
-                right = mid - 1;
-            } else if (time > this.segments[mid].end()) {
-                left = mid + 1;
-            } else {
-                return mid;
-            }
+        public static boolean isParameter(int address) {
+            return (address & PARAMETER_BIT) != 0;
         }
 
-        return left;
+        public static int offset(int address) {
+            return address & ~PARAMETER_BIT;
+        }
     }
 
-    public boolean valid(int index) {
-        return index >= 0 && index < this.max;
-    }
-
-    @Override
-    public @NonNull Iterator<Segment> iterator() {
-        return new ArrayIterator<>(this.segments);
-    }
 }
