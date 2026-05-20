f2676dcc8b02da18bf4f50d27612e63110870493
LouisQuepierts <97670116+LouisQuepierts@users.noreply.github.com>
2026-05-20 18:07:45 +1000

    feat(animation): add parent override and pivot modification parameters

---
+++ src/main/java/net/quepierts/thatskyinteractions/infra/animation/core/skeleton/ParentOverrideParameter.java
@@ -0,0 +1,52 @@
+package net.quepierts.thatskyinteractions.infra.animation.core.skeleton;
+
+import lombok.Getter;
+import lombok.Setter;
+import net.quepierts.thatskyinteractions.infra.animation.backend.skeleton.SkeletonLayout;
+import net.quepierts.thatskyinteractions.infra.animation.backend.uniform.UboDefinition;
+import net.quepierts.thatskyinteractions.infra.animation.backend.uniform.UniformObject;
+import net.quepierts.thatskyinteractions.infra.animation.backend.uniform.UniformBuffer;
+import net.quepierts.thatskyinteractions.infra.animation.backend.uniform.UniformType;
+import net.quepierts.thatskyinteractions.infra.animation.core.model.ParentOverrideConfiguration;
+import org.jspecify.annotations.NonNull;
+
+public final class ParentOverrideParameter extends UniformObject {
+
+    public static ParentOverrideParameter of(@NonNull final SkeletonLayout layout) {
+        final var size = layout.size();
+        final var definition = UboDefinition.builder()
+//                .withArray("order", UniformType.INT, size)
+                .withArray("parent", UniformType.INT, size)
+                .withUniform("enable", UniformType.BOOL)
+                .build();
+        return new ParentOverrideParameter(definition);
+    }
+
+    private final int ADDR_PARENT;
+    private final int LOC_ENABLE;
+
+    @Setter
+    @Getter
+    private ParentOverrideConfiguration data;
+
+    private ParentOverrideParameter(final UboDefinition definition) {
+        super(definition);
+
+        final var locParent = definition.getUniformLocation("parent");
+        this.ADDR_PARENT    = definition.getUniformOffset(locParent);
+        this.LOC_ENABLE     = definition.getUniformLocation("enable");
+    }
+
+    @Override
+    public void upload(final @NonNull UniformBuffer buffer) {
+        if (this.data == null) {
+            buffer.write(this.LOC_ENABLE, 0.0f);
+            return;
+        }
+
+        buffer.write(this.LOC_ENABLE, 1.0f);
+        final var writer = buffer.getRawWriter();
+//        writer.write(0, this.data.getOrder());
+        writer.write(this.ADDR_PARENT, this.data.getParent());
+    }
+}
---
+++ src/main/java/net/quepierts/thatskyinteractions/infra/animation/core/skeleton/PivotModificationParameter.java
@@ -0,0 +1,51 @@
+package net.quepierts.thatskyinteractions.infra.animation.core.skeleton;
+
+import net.quepierts.thatskyinteractions.infra.animation.backend.skeleton.SkeletonLayout;
+import net.quepierts.thatskyinteractions.infra.animation.backend.uniform.UboDefinition;
+import net.quepierts.thatskyinteractions.infra.animation.backend.uniform.UniformObject;
+import net.quepierts.thatskyinteractions.infra.animation.backend.uniform.UniformBuffer;
+import org.jspecify.annotations.NonNull;
+
+public final class PivotModificationParameter extends UniformObject {
+
+    public static PivotModificationParameter of(@NonNull final SkeletonLayout layout) {
+        final var definition = layout.toPivotDefinition();
+        return new PivotModificationParameter(layout, definition);
+    }
+
+    private final SkeletonLayout    layout;
+    private final float[]           pivots;
+
+    private PivotModificationParameter(
+            final @NonNull SkeletonLayout   layout,
+            final @NonNull UboDefinition    definition
+    ) {
+        super(definition);
+        this.layout = layout;
+        this.pivots = new float[layout.size() << 2];
+    }
+
+    public void set(String name, float x, float y, float z) {
+        final var id = this.layout.id(name);
+        if (id != -1) {
+            final var base = id << 2;
+            this.pivots[base] = x;
+            this.pivots[base + 1] = y;
+            this.pivots[base + 2] = z;
+        }
+    }
+
+    public void enable(String name, boolean enable) {
+        final var id = this.layout.id(name);
+        if (id != -1) {
+            final var base = id << 2;
+            this.pivots[base + 3] = enable ? 1.0f : 0.0f;
+        }
+    }
+
+    @Override
+    public void upload(final @NonNull UniformBuffer buffer) {
+        final var writer = buffer.getRawWriter();
+        writer.write(0, this.pivots);
+    }
+}
