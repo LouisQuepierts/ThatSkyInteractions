77c393b9d7600c2a90a65df3cb4bb823ab67537f
LouisQuepierts <97670116+LouisQuepierts@users.noreply.github.com>
2026-05-20 12:45:35 +1000

    feat(skeleton): add pivot pass for skeleton animation transformation

--- src/main/java/net/quepierts/thatskyinteractions/infra/animation/backend/skeleton/pass/SkeletonPass.java
+++ src/main/java/net/quepierts/thatskyinteractions/infra/animation/backend/skeleton/pass/SkeletonPass.java
@@ -1,10 +1,12 @@
 package net.quepierts.thatskyinteractions.infra.animation.backend.skeleton.pass;
 
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import net.quepierts.thatskyinteractions.infra.animation.backend.skeleton.pipeline.SkeletonContext;
 import org.jspecify.annotations.NonNull;
 
+@Getter
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 public abstract class SkeletonPass {
 
