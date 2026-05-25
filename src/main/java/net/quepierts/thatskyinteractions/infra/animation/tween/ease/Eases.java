9dbb3ae89968231ce741acf167a31341b59fe77a
LouisQuepierts <97670116+LouisQuepierts@users.noreply.github.com>
2026-05-25 16:00:16 +1000

    feat(animation): add quadratic and cubic ease functions

--- src/main/java/net/quepierts/thatskyinteractions/infra/animation/tween/ease/Eases.java
+++ src/main/java/net/quepierts/thatskyinteractions/infra/animation/tween/ease/Eases.java
@@ -8,6 +8,12 @@ public class Eases {
 
     public static final Ease LINEAR = t -> t;
 
+    public static final Ease QUAD_IN = t -> t * t;
+
+    public static final Ease QUAD_OUT = t -> t * (2 - t);
+
     public static final Ease CUBIC_IN = t -> t * t * t;
+
+    public static final Ease CUBIC_OUT = t -> --t * t * t + 1;
 
 }
