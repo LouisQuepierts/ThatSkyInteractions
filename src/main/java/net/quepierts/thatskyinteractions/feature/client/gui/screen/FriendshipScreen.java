d41ba1ca8969471cd474b742680f75e06daafaae
LouisQuepierts <97670116+LouisQuepierts@users.noreply.github.com>
2026-05-27 10:49:19 +1000

    refactor(data): remove or invalidate unuse data

---
+++ src/main/java/net/quepierts/thatskyinteractions/feature/client/gui/screen/FriendshipScreen.java
@@ -0,0 +1,9 @@
+package net.quepierts.thatskyinteractions.feature.client.gui.screen;
+
+import net.minecraft.network.chat.Component;
+
+public final class FriendshipScreen extends AnimatableScreen {
+    public FriendshipScreen() {
+        super(Component.translatable("gui.thatskyinteractions.friendship"));
+    }
+}
