3dc2adbfc4eb13dd5431ed0b4eea498ecb82f0f4
LouisQuepierts <97670116+LouisQuepierts@users.noreply.github.com>
2026-06-03 23:49:53 +0800

    feat(animation): add integer interpolation support for tween system

--- src/main/java/net/quepierts/thatskyinteractions/infra/animation/tween/interpolate/Interpolators.java
+++ src/main/java/net/quepierts/thatskyinteractions/infra/animation/tween/interpolate/Interpolators.java
@@ -33,4 +33,21 @@ public class Interpolators {
         return quaternion;
     };
 
+    public static final Interpolator1i INT = (from, to, progress) -> (int) (from + progress * (to - from));
+
+    public static final Interpolator1i PACKED_COLOR = (from, to, progress) -> {
+        int a0 = (from >> 24) & 0xFF;
+        int r0 = (from >> 16) & 0xFF;
+        int g0 = (from >> 8) & 0xFF;
+        int b0 = from & 0xFF;
+        int a1 = (to >> 24) & 0xFF;
+        int r1 = (to >> 16) & 0xFF;
+        int g1 = (to >> 8) & 0xFF;
+        int b1 = to & 0xFF;
+        return (int) (a0 + progress * (a1 - a0)) << 24 |
+                (int) (r0 + progress * (r1 - r0)) << 16 |
+                (int) (g0 + progress * (g1 - g0)) << 8 |
+                (int) (b0 + progress * (b1 - b0));
+    };
+
 }
