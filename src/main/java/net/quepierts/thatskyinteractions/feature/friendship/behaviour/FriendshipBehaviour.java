c77c7f62d803cfec580690ed45d36abd95da112f
LouisQuepierts <97670116+LouisQuepierts@users.noreply.github.com>
2026-06-04 19:42:06 +0800

    refactor(friendship): update FriendshipBehaviour interface to use PlayerFriendshipAttachment

--- src/main/java/net/quepierts/thatskyinteractions/feature/friendship/behaviour/FriendshipBehaviour.java
+++ src/main/java/net/quepierts/thatskyinteractions/feature/friendship/behaviour/FriendshipBehaviour.java
@@ -1,10 +1,10 @@
 package net.quepierts.thatskyinteractions.feature.friendship.behaviour;
 
 import net.minecraft.resources.Identifier;
-import net.minecraft.world.entity.player.Player;
 import net.quepierts.thatskyinteractions.ThatSkyInteractions;
 import net.quepierts.thatskyinteractions.core.friendship.model.FriendshipTreeNode;
 import net.quepierts.thatskyinteractions.core.friendship.model.NodeState;
+import net.quepierts.thatskyinteractions.feature.friendship.PlayerFriendshipAttachment;
 import org.jspecify.annotations.NonNull;
 
 public interface FriendshipBehaviour {
@@ -12,14 +12,14 @@ public interface FriendshipBehaviour {
     @NonNull Identifier DEFAULT_ICON = ThatSkyInteractions.location("textures/icons/none.png");
 
     void execute(
-            final @NonNull  Player              player,
-            final @NonNull  FriendshipTreeNode  node
+            final @NonNull  PlayerFriendshipAttachment  attachment,
+            final @NonNull  FriendshipTreeNode          node
     );
 
     default @NonNull Identifier getIcon(
-            final @NonNull  Player              player,
-            final @NonNull  FriendshipTreeNode  node,
-            final @NonNull  NodeState           state
+            final @NonNull  PlayerFriendshipAttachment  attachment,
+            final @NonNull  FriendshipTreeNode          node,
+            final @NonNull  NodeState                   state
     ) {
         return DEFAULT_ICON;
     }
