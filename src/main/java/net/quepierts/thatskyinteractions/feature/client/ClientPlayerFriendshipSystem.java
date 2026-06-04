08e1a1abc02af4d45619341b0e3b630ef45b0917
LouisQuepierts <97670116+LouisQuepierts@users.noreply.github.com>
2026-06-04 13:41:06 +0800

    feat(friendship): add wait 0.2 seconds before opening friendship screen

--- src/main/java/net/quepierts/thatskyinteractions/feature/client/ClientPlayerFriendshipSystem.java
+++ src/main/java/net/quepierts/thatskyinteractions/feature/client/ClientPlayerFriendshipSystem.java
@@ -10,11 +10,13 @@ import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
 import net.quepierts.thatskyinteractions.ThatSkyInteractions;
+import net.quepierts.thatskyinteractions.feature.animation.tween.PhysicalTweenAttachment;
 import net.quepierts.thatskyinteractions.feature.client.gui.ScreenLoader;
 import net.quepierts.thatskyinteractions.feature.client.gui.screen.FriendshipScreen;
 import net.quepierts.thatskyinteractions.feature.client.input.TSIKeys;
 import net.quepierts.thatskyinteractions.feature.friendship.PlayerFriendshipAttachment;
 import net.quepierts.thatskyinteractions.infra.animation.tween.Tween;
+import net.quepierts.thatskyinteractions.infra.animation.tween.TweenScope;
 
 @UtilityClass
 @EventBusSubscriber(value = Dist.CLIENT, modid = ThatSkyInteractions.MODID)
@@ -46,9 +48,18 @@ public class ClientPlayerFriendshipSystem {
 
         final var tree      = data.get(uuid, PlayerFriendshipAttachment.FRIEND);
 
-        event.setCancellationResult(InteractionResult.SUCCESS);
-        event.setCanceled(true);
-        ScreenLoader.open(FriendshipScreen.class, tree);
+        final var tween     = PhysicalTweenAttachment.tween(player.level());
+        tween               .wait(
+                                () -> {
+                                    if (Minecraft.getInstance().screen == null) {
+                                        ScreenLoader.open(FriendshipScreen.class, tree);
+                                    }
+                                },
+                                0.2f
+                            );
+
+        event               .setCancellationResult(InteractionResult.SUCCESS);
+        event               .setCanceled(true);
     }
 
 }
