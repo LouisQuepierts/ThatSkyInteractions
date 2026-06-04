f2da3c67296465c2d44723649cab9a6e72cd3769
LouisQuepierts <97670116+LouisQuepierts@users.noreply.github.com>
2026-06-04 20:00:23 +0800

    refactor(network): add auto packet registration to data.packet

--- src/main/java/net/quepierts/thatskyinteractions/feature/data/packet/package-info.java
+++ src/main/java/net/quepierts/thatskyinteractions/feature/data/packet/package-info.java
@@ -1,4 +1,4 @@
 @Network
-package net.quepierts.thatskyinteractions.feature.network;
+package net.quepierts.thatskyinteractions.feature.data.packet;
 
 import dev.anvilcraft.lib.v2.network.register.Network;
