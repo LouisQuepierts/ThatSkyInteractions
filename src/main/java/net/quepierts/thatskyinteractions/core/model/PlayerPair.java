bdad2e73ee6ac4fd1a0532c818eff059e4299374
LouisQuepierts <97670116+LouisQuepierts@users.noreply.github.com>
2026-06-05 11:27:04 +0800

    feat(PlayerPair): add validation and utility method for player pairs

--- src/main/java/net/quepierts/thatskyinteractions/core/model/PlayerPair.java
+++ src/main/java/net/quepierts/thatskyinteractions/core/model/PlayerPair.java
@@ -11,7 +11,15 @@ public class PlayerPair {
     @NotNull private final UUID left;
     @NotNull private final UUID right;
 
+    public static PlayerPair of(@NotNull UUID a, @NotNull UUID b) {
+        return new PlayerPair(a, b);
+    }
+
     public PlayerPair(@NotNull UUID a, @NotNull UUID b) {
+        if (a.equals(b)) {
+            throw new IllegalArgumentException("Cannot create pair with same uuid!");
+        }
+
         if (a.compareTo(b) > 0) {
             this.left = a;
             this.right = b;
@@ -19,6 +27,10 @@ public class PlayerPair {
             this.left = b;
             this.right = a;
         }
+    }
+
+    public UUID getOther(UUID uuid) {
+        return uuid.equals(this.left) ? this.right : this.left;
     }
 
     @Override
