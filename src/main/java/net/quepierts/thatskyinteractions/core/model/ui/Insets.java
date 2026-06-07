73e87a38fa23261bb83ba8030203e472b450a2e5
LouisQuepierts <97670116+LouisQuepierts@users.noreply.github.com>
2026-06-07 20:58:48 +0800

    feat(ui): add setter methods to Insets class

--- src/main/java/net/quepierts/thatskyinteractions/core/model/ui/Insets.java
+++ src/main/java/net/quepierts/thatskyinteractions/core/model/ui/Insets.java
@@ -16,4 +16,20 @@ public final class Insets {
     public Insets(float all) {
         this(all, all, all, all);
     }
+
+    public void set(float left, float top, float right, float bottom) {
+        this.left = left;
+        this.top = top;
+        this.right = right;
+        this.bottom = bottom;
+    }
+
+    public void set(float all) {
+        this.set(all, all, all, all);
+    }
+
+    public void set(float h, float v) {
+        this.set(h, v, h, v);
+    }
+
 }
