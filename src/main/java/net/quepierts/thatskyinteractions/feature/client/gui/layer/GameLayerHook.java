24e201f7416b5c896e11713508598406b537c0b3
LouisQuepierts <97670116+LouisQuepierts@users.noreply.github.com>
2026-06-07 17:13:32 +0800

    feat(gui): register floating control layer in game

--- src/main/java/net/quepierts/thatskyinteractions/feature/client/gui/layer/GameLayerHook.java
+++ src/main/java/net/quepierts/thatskyinteractions/feature/client/gui/layer/GameLayerHook.java
@@ -5,9 +5,23 @@ import lombok.experimental.UtilityClass;
 import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphicsExtractor;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.client.event.RegisterGuiLayersEvent;
+import net.quepierts.thatskyinteractions.ThatSkyInteractions;
+
 
 @UtilityClass
+@EventBusSubscriber(value = Dist.CLIENT, modid = ThatSkyInteractions.MODID)
 public class GameLayerHook {
+
+
+    @SubscribeEvent
+    public static void onRegisterGuiLayers(final RegisterGuiLayersEvent event) {
+        event.registerAboveAll(FloatingControlLayer.IDENTIFIER, FloatingControlLayer.INSTANCE);
+    }
+
 
     public static void onRenderLayer(
             @NonNull final GuiGraphicsExtractor graphics,
